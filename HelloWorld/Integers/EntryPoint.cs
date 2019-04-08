a1fa914cbef6e84a8f68aa9d69d97395e6e6c7aa
ndelforno <nicolas.delforno@gmail.com>
2019-04-08 16:14:01 -0400

    integers file created

---
+++ HelloWorld/Integers/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        int someInteger = 5;
+        Console.WriteLine(someInteger);
+    }
+}
+
--- HelloWorld/Integers/Properties/AssemblyInfo.cs
+++ HelloWorld/Integers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Integers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Integers")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e7765ba4-202e-49d7-b436-df3b1dbd5be4")]
+[assembly: Guid("bda51f85-b4a4-40ef-9f87-70283be48765")]
 
 // Version information for an assembly consists of the following four values:
 //
