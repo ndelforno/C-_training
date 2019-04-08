bb6528d0dac2d1244bc3ef9b52ea9b4492bc65cc
nicolas.delforno <nicolas.delforno@intelex.com>
2019-04-08 14:04:26 -0400

    User prompt

--- HelloWorld/HelloWorld/EntryPoint.cs
+++ HelloWorld/HelloWorld/EntryPoint.cs
@@ -6,6 +6,12 @@ class EntryPoint
     static void Main()
     {
         Console.WriteLine("Hello World");
+        Console.WriteLine("");
+        Console.WriteLine("what is your name ?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine(firstName);
+
+
     }
  }
 
