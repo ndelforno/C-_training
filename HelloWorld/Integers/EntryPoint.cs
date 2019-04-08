f5e65022008cff67105a65ed6e8ca840a6f3b907
ndelforno <nicolas.delforno@gmail.com>
2019-04-08 16:26:51 -0400

    integers tuto completed

--- HelloWorld/Integers/EntryPoint.cs
+++ HelloWorld/Integers/EntryPoint.cs
@@ -5,8 +5,32 @@ class EntryPoint
 {
     static void Main()
     {
+        // int type holds 32 bits numbers
         int someInteger = 5;
         Console.WriteLine(someInteger);
+        Console.WriteLine(int.MinValue);
+        Console.WriteLine(int.MaxValue);
+
+        uint someUinteger = 1; // unsigned int
+        Console.WriteLine(uint.MinValue);
+        Console.WriteLine(uint.MaxValue);
+
+        // byte type holds 8bit number
+
+        byte someByte = 200;
+
+        sbyte negativeByte = -1; // signed byte -128 to 127
+
+        Console.WriteLine(byte.MinValue);
+        Console.WriteLine(byte.MaxValue);
+        Console.WriteLine(sbyte.MinValue);
+        Console.WriteLine(sbyte.MaxValue);
+
+        long someverylongnumber = 15364646132156;
+        Console.WriteLine(long.MinValue);
+        Console.WriteLine(long.MaxValue);
+
+
     }
 }
 
