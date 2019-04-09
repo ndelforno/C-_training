7a537236c3970507d5300dabb34d8ba2373c835c
ndelforno <nicolas.delforno@gmail.com>
2019-04-09 09:06:25 -0400

    writeLine & write

---
+++ HelloWorld/HelloWorld/WriteAndWriteLine/Entrypoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Entrypoint
+{
+    static void Main()
+    {
+        Console.Write("I am learning");
+        Console.Write("C# from");
+        Console.Write("Udemy");
+    }
+}
+
