e8ec9ba2ff959709672fac2be889bf31867ab645
ndelforno <nicolas.delforno@gmail.com>
2019-04-09 09:58:19 -0400

    Basic arythmetic operators done

---
+++ HelloWorld/HelloWorld/ArythmeticOperators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int firstNumber = 20;
+        int secondNumber = 7;
+
+        int addition = firstNumber + secondNumber;
+        int substraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double division = (double)firstNumber / (double)secondNumber;
+
+        Console.WriteLine(addition);
+        Console.WriteLine(substraction);
+        Console.WriteLine(multiplication);
+        Console.WriteLine(division);
+
+        firstNumber++;
+        Console.WriteLine(firstNumber);
+
+        Console.WriteLine(551 % 2);
+    }
+}
