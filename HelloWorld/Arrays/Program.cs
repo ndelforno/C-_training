b7f6d1e68d0265296775c96ad38d4024efa98f5b
ndelforno <nicolas.delforno@gmail.com>
2019-04-09 08:56:16 -0400

    Array part 1 done

--- HelloWorld/Arrays/Program.cs
+++ HelloWorld/Arrays/Program.cs
@@ -8,6 +8,12 @@ class Program
         string iamArray = "array";
         Console.WriteLine(iamArray[3]);
 
-        string[] iamStringArray = new string[3];
+        string[] iamStringArray = {"4543", "bhkih", "45342", "41543" };
+        Console.WriteLine(iamStringArray[1]);
+        Console.WriteLine(iamStringArray.Length);
+
+        int[] integerArray = { 1, 2, 3, 3, 3, 5, 4 };
+        Console.WriteLine(integerArray[4]);
+ 
     }
 }
