25b83828235201b199b145db25b5855cbc20496e
ndelforno <nicolas.delforno@gmail.com>
2019-04-16 16:40:56 -0400

    change conditionals statements folder to project

---
+++ HelloWorld/ConsoleApp1/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        if (4 < 5)
+        {
+            Console.WriteLine("for is smaller than 5");
+        }
+    }
+}
--- HelloWorld/Strings/conditional statements/EntryPoint.cs
+++
@@ -1,7 +0,0 @@
-using System;
-{
-    static void Main()
-    {
-
-    }
-}
