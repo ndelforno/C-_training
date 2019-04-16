1741eea7d0dc4e49780218e8e5ee86ad6a7cd596
ndelforno <nicolas.delforno@gmail.com>
2019-04-16 17:00:41 -0400

    while loop done

---
+++ HelloWorld/HelloWorld/ConsoleApp1/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        int count = 0;
+        
+        while (count < 10)
+        {
+            Console.WriteLine(count);
+            count += 1;
+        }
+    }
+}
